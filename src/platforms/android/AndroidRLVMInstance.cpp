8c88fc91404d98a858f538c150904fe5e2c3f47d
Ilya Zhuravlev <whatever@xyz.is>
2018-01-26 20:01:46 -0500

    Implement ReportFatalError

--- src/platforms/android/AndroidRLVMInstance.cpp
+++ src/platforms/android/AndroidRLVMInstance.cpp
@@ -1,5 +1,7 @@
 #include "AndroidRLVMInstance.hpp"
 
+#include <SDL2/SDL.h>
+
 bool AndroidRLVMInstance::AskUserPrompt(const std::string& message_text,
                                         const std::string& informative_text,
                                         const std::string& true_button,
@@ -12,4 +14,10 @@ bool global_texture_reload = false;
 
 void AndroidRLVMInstance::ReloadAllTextures() {
 	global_texture_reload = true;
+}
+
+void AndroidRLVMInstance::ReportFatalError(const std::string& message_text,
+                                    const std::string& informative_text) {
+  std::string message = message_text + ": " + informative_text;
+  SDL_ShowSimpleMessageBox(0, "rlvm: Fatal error", message.c_str(), NULL);
 }
--- src/platforms/android/AndroidRLVMInstance.hpp
+++ src/platforms/android/AndroidRLVMInstance.hpp
@@ -4,6 +4,9 @@ class AndroidRLVMInstance : public RLVMInstance {
  public:
   void ReloadAllTextures();
  protected:
+  virtual void ReportFatalError(const std::string& message_text,
+                                const std::string& informative_text);
+
   virtual bool AskUserPrompt(const std::string& message_text,
                              const std::string& informative_text,
                              const std::string& true_button,
